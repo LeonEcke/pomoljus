9f7a4a6f1558539309c40759f26ba048d48a9533
TheMoo96 <leon.eckerbom@gmail.com>
2023-08-30 18:54:26 +0200

    Add comments and remove magic numbers

--- firmware/include/ledModes/ledModes.hpp
+++ firmware/include/ledModes/ledModes.hpp
@@ -9,16 +9,29 @@ Loop into one of these to progress animation.
 Make sure tickMs is kept updated as well.
 */
 
+/// @brief Sets pointers used by led modifier functions
+/// @param led_array_pointer Pointer to first index in led array
+/// @param tick_pointer Pointer to tick counter. OBS in milliseconds!
 void light_mode_setup(HSV* led_array_pointer, long* tick_pointer);
 
+/// @brief A stronger glow and rapid breathing red and orange
 void alarm_light_mode();
 
+/// @brief A calmer breathing animation of red and orange
 void alarm_overtime_light_mode();
 
+/// @brief A blue progress bar with slight hue shifts
+/// @param progress `]0, 255[` value of progress completed.
 void work_light_mode(uint8_t progress);
 
+
+/// @brief A green progress bar with slight hue shifts
+/// @param progress `]0, 255[` value of progress completed.
 void short_rest_light_mode(uint8_t progress);
 
+
+/// @brief A yellow progress bar with slight hue shifts
+/// @param progress `]0, 255[` value of progress completed.
 void long_rest_light_mode(uint8_t progress);
 
 #endif
--- firmware/src/ledModes.cpp
+++ firmware/src/ledModes.cpp
@@ -4,6 +4,7 @@
 #include "ledValue/ledValue.hpp"
 #include "pico/stdlib.h"
 
+static const uint8_t byte_max_value = 0xFF;
 uint8_t default_value = 100;
 uint8_t alarm_value = 200;
 
@@ -21,23 +22,29 @@ void light_mode_setup(HSV* led_array_pointer, long* tick_pointer) {
 }
 
 void alarm_light_mode() {
-	const static HSV background = {orange, 255, alarm_value};
+	const static HSV background = {orange, byte_max_value, alarm_value};
 	setColour(background);
+	
+	// most litterals below are based purely on feel
 	breathAnimation(700, alarm_value - (alarm_value / 7), 0, changeValue);
 	waveAnimation(500, 17, 300, changeHue);
 }
 
 void alarm_overtime_light_mode() {
-	const static HSV background = {orange, 255, default_value};
+	const static HSV background = {orange, byte_max_value, default_value};
 	setColour(background);
+	
+	// most litterals below are based purely on feel
 	waveAnimation(1000, 17, 300, changeHue);
 	breathAnimation(5000, default_value - (default_value / 4), 0, changeValue);
 }
 
 /// @brief Generic progress function for internal use.
 void generic_progress_light_mode(uint8_t progress, uint8_t background_hue) {
-	const static HSV background = {background_hue, 255, default_value};
+	const static HSV background = {background_hue, byte_max_value, default_value};
 	setColour(background);
+	
+	// most litterals below are based purely on feel
 	waveAnimation(1000, 10, 700, changeHue);
 	progressAnimation(progress, default_value - (default_value / 4),
 					  changeValue);

596bb396bddca2db554071dedc824a0d3a181481
TheMoo96 <leon.eckerbom@gmail.com>
2023-08-30 15:58:19 +0200

    Refactor work mode and implement other progresses
    Internal generic is now used for all main progresser modes.

--- firmware/src/ledModes.cpp
+++ firmware/src/ledModes.cpp
@@ -18,9 +18,22 @@ void light_mode_setup(HSV* led_array_pointer, long* tick_pointer) {
 	setPointers( led_array_pointer, tick_pointer);
 }
 
-void work_light_mode(uint8_t progress) {
-	const static HSV background = {blue, 255, default_value};
+/// @brief Generic progress function for internal use.
+void generic_progress_light_mode(uint8_t progress, uint8_t background_hue){
+	const static HSV background = {background_hue, 255, default_value};
 	setColour(background);
 	waveAnimation(1000, 10, 700, changeHue);
-	progressAnimation(progress, default_value, changeValue);
+	progressAnimation(progress, default_value - (default_value / 7), changeValue);
+}
+
+void work_light_mode(uint8_t progress) {
+	generic_progress_light_mode(progress, blue);
+}
+
+void short_rest_light_mode(uint8_t progress) {
+	generic_progress_light_mode(progress, green);
+}
+
+void long_rest_light_mode(uint8_t progress) {
+	generic_progress_light_mode(progress, yellow);
 }
